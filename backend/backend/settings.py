91a16ce09a599167454987041ad88c0092b483f6
SahilLodhe <sahillodhe17@gmail.com>
2023-04-22 22:05:56 +0530

    fixed a minor bug missing corsheaders

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # 'corsheader.middleware.CorsMiddleware',
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
 ]
